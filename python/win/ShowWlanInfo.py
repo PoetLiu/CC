28e4592ab6c2e72317ebd96c87a94b5525328983
PoetLiu <457566268@qq.com>
2015-10-12 10:24:58 +0800

    python show wifi info.

--- python/win/ShowWlanInfo.py
+++ python/win/ShowWlanInfo.py
@@ -1,9 +1,25 @@
-#    *--encoding:utf-8*#
+#  encoding:utf-8
+#   Base on python 2.7.9
 from ctypes import *
 from ctypes.wintypes import *
 from sys import exit
 def customresize(array, new_size):
     return (array._type_ * new_size).from_address(addressof(array))
+def is_chinese(uchar):
+    if uchar >= u'\u4e00' and uchar <= u'\u9fa5':
+        return True
+    else:
+        return False
+ 
+def fill_text_to_print_width(text, width):
+    stext = str(text)
+    utext = stext.decode("utf-8")
+    cn_count = 0
+    for u in utext:
+        if is_chinese(u):
+            cn_count += 1
+    return utext + " " * (width - cn_count - len(utext)) 
+
 wlanapi = windll.LoadLibrary('wlanapi.dll')
 ERROR_SUCCESS = 0
 class GUID(Structure):
@@ -21,7 +37,7 @@ WLAN_INTERFACE_STATE = c_uint
     wlan_interface_state_disconnected,
     wlan_interface_state_associating,
     wlan_interface_state_discovering,
-    wlan_interface_state_authenticating) = map(WLAN_INTERFACE_STATE, xrange(0, 8))
+    wlan_interface_state_authenticating) = map(WLAN_INTERFACE_STATE, range(0, 8))
 class WLAN_INTERFACE_INFO(Structure):
     _fields_ = [
         ("InterfaceGuid", GUID),
@@ -40,7 +56,7 @@ WLAN_REASON_CODE = DWORD
 DOT11_BSS_TYPE = c_uint
 (dot11_BSS_type_infrastructure,
     dot11_BSS_type_independent,
-    dot11_BSS_type_any) = map(DOT11_BSS_TYPE, xrange(1, 4))
+    dot11_BSS_type_any) = map(DOT11_BSS_TYPE, range(1, 4))
 DOT11_PHY_TYPE = c_uint
 dot11_phy_type_unknown = 0
 dot11_phy_type_any = 0
@@ -78,6 +94,30 @@ WLAN_AVAILABLE_NETWORK_CONNECTED = 1
 WLAN_AVAILABLE_NETWORK_HAS_PROFILE = 2
 WLAN_AVAILABLE_NETWORK_INCLUDE_ALL_ADHOC_PROFILES = 0x00000001
 WLAN_AVAILABLE_NETWORK_INCLUDE_ALL_MANUAL_HIDDEN_PROFILES = 0x00000002
+
+
+def print_auth(auth):
+    auth_msg = {
+         DOT11_AUTH_ALGO_80211_OPEN : "OPEN",
+         DOT11_AUTH_ALGO_80211_SHARED_KEY :"SHARED_KEY",
+         DOT11_AUTH_ALGO_WPA : "WPA",
+         DOT11_AUTH_ALGO_WPA_PSK  : "WPA_PSK",
+         DOT11_AUTH_ALGO_WPA_NONE  : "WPA_NONE",
+         DOT11_AUTH_ALGO_RSNA  : "RSNA",
+         DOT11_AUTH_ALGO_RSNA_PSK  : "RSNA_PSK",
+    }
+    return auth_msg.get(auth)
+        
+def freq_2_chn(freq):
+    if (freq >= 5000000):
+        chn = (freq % 5000000) / 1000
+        chn = chn / 5
+    else:
+        chn = (freq % 2412000) / 1000
+        chn = chn / 5
+
+    return chn + 1
+
 class DOT11_SSID(Structure):
     _fields_ = [
         ("SSIDLength", c_ulong),
@@ -107,6 +147,59 @@ class WLAN_AVAILABLE_NETWORK_LIST(Structure):
         ("Index", DWORD),
         ("Network", WLAN_AVAILABLE_NETWORK * 1)
         ]
+
+DOT11_MAC_ADDRESS = c_ubyte * 6
+DOT11_CIPHER_ALGORITHM = c_uint
+DOT11_CIPHER_ALGO_NONE   = 0x00
+DOT11_CIPHER_ALGO_WEP40   = 0x01
+DOT11_CIPHER_ALGO_TKIP   = 0x02
+DOT11_PHY_TYPE = c_uint
+DOT11_PHY_TYPE_UNKNOWN = 0
+DOT11_PHY_TYPE_ANY   = 0
+DOT11_PHY_TYPE_FHSS  = 1
+DOT11_PHY_TYPE_DSSS  = 2
+DOT11_PHY_TYPE_IRBASEBAND = 3
+DOT11_PHY_TYPE_OFDM  = 4
+DOT11_PHY_TYPE_HRDSSS  = 5
+DOT11_PHY_TYPE_ERP   = 6
+DOT11_PHY_TYPE_HT   = 7
+DOT11_PHY_TYPE_IHV_START = 0X80000000
+DOT11_PHY_TYPE_IHV_END  = 0XFFFFFFFF
+class WLAN_RATE_SET(Structure):
+    _fields_ = [
+        ("uRateSetLength", c_ulong),
+        ("usRateSet", c_ushort * 126)
+]
+class WLAN_BSS_ENTRY(Structure):
+    _fields_ = [
+        ("dot11Ssid",DOT11_SSID),
+        ("uPhyId",c_ulong),
+        ("dot11Bssid", DOT11_MAC_ADDRESS),
+        ("dot11BssType", DOT11_BSS_TYPE),
+        ("dot11BssPhyType", DOT11_PHY_TYPE),
+        ("lRssi", c_long),
+        ("uLinkQuality", c_ulong),
+        ("bInRegDomain", c_bool),
+        ("usBeaconPeriod",c_ushort),
+        ("ullTimestamp", c_ulonglong),
+        ("ullHostTimestamp",c_ulonglong),
+        ("usCapabilityInformation",c_ushort),
+        ("ulChCenterFrequency", c_ulong),
+        ("wlanRateSet",WLAN_RATE_SET),
+        ("ulIeOffset", c_ulong),
+        ("ulIeSize", c_ulong)]
+class WLAN_BSS_LIST(Structure):
+    _fields_ = [
+        ("TotalSize", DWORD),
+        ("NumberOfItems", DWORD),
+        ("NetworkBSS", WLAN_BSS_ENTRY * 1)
+ ]
+class WLAN_AVAILABLE_NETWORK_LIST_BSS(Structure):
+    _fields_ = [
+        ("TotalSize", DWORD),
+        ("NumberOfItems", DWORD),
+        ("Network", WLAN_BSS_ENTRY * 1)
+ ]
 WlanOpenHandle = wlanapi.WlanOpenHandle
 WlanOpenHandle.argtypes = (DWORD, c_void_p, POINTER(DWORD), POINTER(HANDLE))
 WlanOpenHandle.restype = DWORD
@@ -118,6 +211,10 @@ WlanGetAvailableNetworkList = wlanapi.WlanGetAvailableNetworkList
 WlanGetAvailableNetworkList.argtypes = (HANDLE, POINTER(GUID), DWORD, c_void_p,
                                         POINTER(POINTER(WLAN_AVAILABLE_NETWORK_LIST)))
 WlanGetAvailableNetworkList.restype = DWORD
+WlanGetNetworkBssList = wlanapi.WlanGetNetworkBssList
+WlanGetNetworkBssList.argtypes = (HANDLE, POINTER(GUID), POINTER(DOT11_SSID), DOT11_BSS_TYPE, c_bool, c_void_p,
+                                  POINTER(POINTER(WLAN_BSS_LIST)))
+WlanGetNetworkBssList.restype = DWORD
 WlanFreeMemory = wlanapi.WlanFreeMemory
 WlanFreeMemory.argtypes = [c_void_p]
 if __name__ == '__main__':
@@ -136,7 +233,7 @@ if __name__ == '__main__':
                                 pInterfaceList.contents.NumberOfItems)
         #find each available network for each interface
         for iface in ifaces:
-            print "Interface: %s" % (iface.strInterfaceDescription)
+            print( "Interface: %s" % (iface.strInterfaceDescription))
             pAvailableNetworkList = pointer(WLAN_AVAILABLE_NETWORK_LIST())
             ret = WlanGetAvailableNetworkList(ClientHandle,
                                         byref(iface.InterfaceGuid),
@@ -149,11 +246,33 @@ if __name__ == '__main__':
                 avail_net_list = pAvailableNetworkList.contents
                 networks = customresize(avail_net_list.Network,
                                         avail_net_list.NumberOfItems)
+                print ("%-33s%-10s%-7s%-9s%-11s%-12s%-18s" % ("[SSID]","[Quality]","[RSSI]","[Freq]","[Channel]","[Auth]","[BSSID]"))
                 for network in networks:
-                    print "[SSID: %s]\t[quality: %02d%%]" % (
-                        network.dot11Ssid.SSID[:network.dot11Ssid.SSIDLength].decode("utf-8"),
-                        network.wlanSignalQuality)
-                       # network.dot11Ssid)
+                    pAvailableNetworkList2 = pointer(WLAN_BSS_LIST())
+                    ret2 = WlanGetNetworkBssList(ClientHandle,
+                                                 byref(iface.InterfaceGuid), 
+                                                 network.dot11Ssid,
+                                                 network.dot11BssType,
+                                                 True,
+                                                 None,
+                                                 byref(pAvailableNetworkList2))
+                    if ret2 != ERROR_SUCCESS:
+                            exit(FormatError(ret2))
+                    avail_net_list2 = pAvailableNetworkList2.contents
+                    networks2 = customresize(avail_net_list2.NetworkBSS,
+                                             avail_net_list2.NumberOfItems)
+                    for network1 in networks2:
+                        Bssid   = ':'.join('%02x' % b for b in network1.dot11Bssid).upper()
+                        Rssi    = str(network1.lRssi)
+                        Freq    = network1.ulChCenterFrequency
+                    print ("%s%-10d%-7s%-9d%-11s%-12s%-18s" % (
+                       fill_text_to_print_width(network.dot11Ssid.SSID[:network.dot11Ssid.SSIDLength],33),
+                       network.wlanSignalQuality,
+                       Rssi,
+                       Freq,
+                       freq_2_chn(Freq),
+                       print_auth(network.dot11DefaultAuthAlgorithm),
+                       Bssid))
                     
             finally:
                 WlanFreeMemory(pAvailableNetworkList)
