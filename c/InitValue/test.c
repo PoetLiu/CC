388b0b3460bc4686c4ade49ad6d8ec85571d07d3
PoetLiu <457566268@qq.com>
2015-11-27 09:44:47 +0800

    Md5 test

---
+++ c/InitValue/test.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void main_loop(int argc, char **argv)
+{
+	int i;
+
+	while (i < argc) {
+		
+		i--;
+	}
+}
+
+int main(int argc, char *argv[])
+{
+	main_loop(argc, argv);
+	return 0;
+}
