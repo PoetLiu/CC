619a0ee078e51532f29adce6428f0b30f8184272
liupeng <457566268@qq.com>
2018-03-13 17:19:28 +0800

    Add Macro test code.

--- c/Basic/Macro/func_macro.c
+++ c/Basic/Macro/func_macro.c
@@ -13,10 +13,31 @@ NF_HOOK(unsigned char *buf, void (*okfn)(unsigned char *buf1)) {
 
 #define NF_HOOK(buf, okfn) (okfn)(buf)
 
+#define FUN_CALL_RETRY(func, t, ret) {	\
+		{\
+			int call_cnt = 0;\
+			while (call_cnt <= t) {\
+				ret = func;\
+				if (ret != 0) {\
+					usleep(500000);\
+					call_cnt++;\
+				} else {\
+					break;\
+				}\
+			}\
+		}\
+}	
+
+int test(char *msg) {
+	printf("%s\n", msg);
+	return -1;
+}
 int main(void)
 {
 	char *p = "hello, world!\n";
+	int ret = 0;
 	NF_HOOK(p, myfunc);
+	FUN_CALL_RETRY(test("fuck"), 2, ret);
 }
 
 
